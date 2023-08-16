cd78afb029322faeb125d47b2acedc6d1e06da0e
lollo03 <03lollo@gmail.com>
2023-08-16 09:37:52 +0200

    refactor and small improvements

--- src/components/name_selector.vue
+++ src/components/name_selector.vue
@@ -27,48 +27,9 @@ input {
   background-color: red;
 }
 
-.popup {
-  background-color: white;
-  width: fit-content;
-  text-align: center;
-  border-radius: 6px;
-  padding: 8px 0;
-  position: absolute;
-  z-index: 1;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-@media (prefers-color-scheme: dark) {
-  .popup {
-    background-color: rgba(0, 0, 0, 0.5);
-  }
-}
-
 .row {
   display: flex;
   flex-direction: row;
   align-items: center;
-}
-
-button {
-  background-color: #622f8a; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 10px;
-  border-radius: 5px;
-  transition: 500ms;
-}
-button:hover {
-  transform: scale(1.2);
 }
 </style>
